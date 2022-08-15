0201257abd94f8ea0025c1b47e68bb8418ecd80a
Andres Acuna <andres.acu1997@gmail.com>
2022-08-15 15:59:56 -0500

    Google Sign in and Log out Done

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,6 +3,7 @@ const bcryptjs = require("bcryptjs");
 
 const User = require("../models/user");
 const { generateJWT } = require("../helpers/generateJWT");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
   const { email, password } = req.body;
@@ -43,4 +44,44 @@ const login = async (req, res = response) => {
   }
 };
 
-module.exports = { login };
+const googleSignIn = async (req, res = response) => {
+  const { id_token } = req.body;
+
+  try {
+    const { name, picture, email } = await googleVerify(id_token);
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      //Create user if does not exist
+      const data = { name, email, picture, password: "ASDA", google: true, role:'USER_ROLE' };
+
+      user = new User(data);
+      await user.save();
+    }
+
+    if (!user.state) {
+      res.status(401).json({
+        ok: false,
+        msg: `The user is Unauthorized`,
+      });
+    }
+
+    //Create JWT
+    const token = await generateJWT(user.id);
+
+    res.json({
+      ok: true,
+      user,
+      token,
+    });
+
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: `Error ${error}`,
+    });
+  }
+};
+
+module.exports = { login, googleSignIn };
